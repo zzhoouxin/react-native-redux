b9a6d961264851c6df21c4ae704671edca1373a2
zhouxin <zxh87516@qq.com>
2019-06-28 11:34:10 +0800

    测试

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended","eslint-config-prettier"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react","prettier"
+    ],
+    "rules": {
+    }
+};
